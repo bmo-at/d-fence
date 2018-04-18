db3ce3dd2deeb8a01f8381fcc5b574cdc02a57af
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-04-18 23:08:44 +0200

    Add touch listener and angular calc

--- d-fence/GameScene.swift
+++ d-fence/GameScene.swift
@@ -8,13 +8,69 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let scout : SKSpriteNode = SKSpriteNode(imageNamed: "scout")
+    let background : SKSpriteNode = SKSpriteNode(imageNamed: "background")
+    
     override func didMove(to view: SKView) {
-        let background = SKSpriteNode(imageNamed: "background")
+        backgroundColor = UIColor.green // replace with init background when assets are ready
+        
+        renderScout()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch:UITouch = touches.first!
+        let positionInScene = touch.location(in: self)
+        let touchedNode = self.atPoint(positionInScene)
+        
+        print()
+        
+        if let name = touchedNode.name {
+            print(name)
+            if name == "scout" {
+                print("User clicked scout")
+            }
+        } else {
+            print("User clicked anything else...")
+            updateScoutRotation(tPoint: positionInScene)
+        }
+    }
+    
+    func vector_dot(a: CGPoint, b: CGPoint) -> CGFloat {
+        return a.x * b.x + a.y * b.y
+    }
+    
+    func vector_norm(a: CGPoint) -> CGFloat {
+        return sqrt(vector_dot(a: a, b: a))
+    }
+    
+    func updateScoutRotation(tPoint: CGPoint) {
+        let a = CGPoint(x: 1, y: 0)
+        let t = CGPoint(x: tPoint.x - scout.position.x, y: tPoint.y - scout.position.y)
+        
+        let phi = acos(vector_dot(a: a, b: t) / (vector_norm(a: a) * vector_norm(a: t)))
+        
+        scout.zRotation = phi;
+        
+        print(a)
+        print(t)
+        print(phi * 180 / .pi)
+    }
+    
+    func initBackground() {
         background.anchorPoint = CGPoint.zero
         background.position = CGPoint.zero
         background.zPosition = -1
         
         addChild(background)
+    }
+    
+    func renderScout() {
+        scout.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        scout.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        scout.zPosition = 10
+        scout.name = "scout"
+        
+        addChild(scout)
     }
     
 }
