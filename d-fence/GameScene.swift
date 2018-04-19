1d0fce65d497df1b1a6fd813d8c4e98e141baf57
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-04-19 18:14:08 +0200

    Calc direction of bullets

--- d-fence/GameScene.swift
+++ d-fence/GameScene.swift
@@ -11,10 +11,10 @@ class GameScene: SKScene {
     let touchDebug = true
     let scout : SKSpriteNode = SKSpriteNode(imageNamed: "scout")
     let background : SKSpriteNode = SKSpriteNode(imageNamed: "background")
-    var shots : [SKSpriteNode] = []
+    var shots = [SKSpriteNode: CGPoint]()
     
     var bulletMovePointsPerSecond: CGFloat = 480.0
-    var velocity = CGPoint.zero
+    var bulletVelocity: CGFloat = 4
     
     var lastUpdateTime: TimeInterval = 0
     var dt: TimeInterval = 0
@@ -38,8 +38,8 @@ class GameScene: SKScene {
         print("\(dt*1000) milliseconds since last update")
         
         
-        for shot in shots {
-            shot.position = CGPoint(x: shot.position.x + 8, y: shot.position.y)
+        for (shot, direction) in shots {
+            shot.position = CGPoint(x: shot.position.x + direction.x, y: shot.position.y + direction.y)
         }
         
     }
@@ -57,8 +57,8 @@ class GameScene: SKScene {
             }
         } else {
             touchDebug("User clicked anything else...")
-            updateScoutRotation(tPoint: positionInScene)
-            initShot(tPoint: positionInScene)
+            updateScoutRotation(touchPoint: positionInScene)
+            initShot(touchPoint: positionInScene)
         }
     }
     
@@ -74,7 +74,7 @@ class GameScene: SKScene {
             }
         } else {
             touchDebug("User is moving finger over anything else")
-            updateScoutRotation(tPoint: positionInScene)
+            updateScoutRotation(touchPoint: positionInScene)
         }
     }
     
@@ -84,39 +84,35 @@ class GameScene: SKScene {
         }
     }
     
-    func vectorDot(a: CGPoint, b: CGPoint) -> CGFloat {
-        return a.x * b.x + a.y * b.y
+    func vectorDot(vectorA: CGPoint, vectorB: CGPoint) -> CGFloat {
+        return vectorA.x * vectorB.x + vectorA.y * vectorB.y
     }
     
-    func vectorNorm(a: CGPoint) -> CGFloat {
-        return sqrt(vectorDot(a: a, b: a))
+    func vectorAbs(vector: CGPoint) -> CGFloat {
+        return sqrt(vectorDot(vectorA: vector, vectorB: vector))
     }
     
-    func updateScoutRotation(tPoint: CGPoint) {
-        let a = CGPoint(x: 1, y: 0)
-        let t = CGPoint(x: tPoint.x - scout.position.x, y: tPoint.y - scout.position.y)
-        
-        let phi = acos(vectorDot(a: a, b: t) / (vectorNorm(a: a) * vectorNorm(a: t)))
-        
-        // as scalar dot only returns angulars smaller 180 degrees, negate on big angulars
-        scout.zRotation = t.y > 0 ? phi : -phi;
+    func vectorNorm(vector: CGPoint) -> CGPoint {
+        let abs = vectorAbs(vector: vector)
+        return CGPoint(x: vector.x / abs, y: vector.y / abs)
     }
     
-    func initBackground() {
-        background.anchorPoint = CGPoint.zero
-        background.position = CGPoint.zero
-        background.zPosition = -1
-        
-        addChild(background)
+    func vectorScale(vector: CGPoint, scale: CGFloat) -> CGPoint {
+        return CGPoint(x: vector.x * scale, y: vector.y * scale)
     }
     
-    func initShot(tPoint: CGPoint) {
+    func calculateDirectionOfShot(touchPoint: CGPoint) -> CGPoint {
+        let difference = CGPoint(x: touchPoint.x - scout.position.x, y: touchPoint.y - scout.position.y)
+        return vectorScale(vector: vectorNorm(vector: difference), scale: bulletVelocity)
+    }
+    
+    func initShot(touchPoint: CGPoint) {
         let newShot = SKSpriteNode(imageNamed: "bullet")
         newShot.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         newShot.position = scout.position
         newShot.zPosition = 20
         
-        shots.append(newShot)
+        shots[newShot] = calculateDirectionOfShot(touchPoint: touchPoint)
 
         addChild(newShot)
     }
@@ -130,6 +126,21 @@ class GameScene: SKScene {
         addChild(scout)
     }
     
+    func updateScoutRotation(touchPoint: CGPoint) {
+        let a = CGPoint(x: 1, y: 0)
+        let t = CGPoint(x: touchPoint.x - scout.position.x, y: touchPoint.y - scout.position.y)
+        
+        let phi = acos(vectorDot(vectorA: a, vectorB: t) / (vectorAbs(vector: a) * vectorAbs(vector: t)))
+        
+        // as scalar dot only returns angulars smaller 180 degrees, negate on big angulars
+        scout.zRotation = t.y > 0 ? phi : -phi;
+    }
     
-    
+    func initBackground() {
+        background.anchorPoint = CGPoint.zero
+        background.position = CGPoint.zero
+        background.zPosition = -1
+        
+        addChild(background)
+    }
 }
