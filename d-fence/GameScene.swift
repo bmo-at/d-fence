e455480548919c73c97524d65f88ebc76df05eef
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-04-19 14:05:19 +0200

    Add moving capabilities

--- d-fence/GameScene.swift
+++ d-fence/GameScene.swift
@@ -22,8 +22,6 @@ class GameScene: SKScene {
         let positionInScene = touch.location(in: self)
         let touchedNode = self.atPoint(positionInScene)
         
-        print()
-        
         if let name = touchedNode.name {
             print(name)
             if name == "scout" {
@@ -31,6 +29,22 @@ class GameScene: SKScene {
             }
         } else {
             print("User clicked anything else...")
+            updateScoutRotation(tPoint: positionInScene)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch:UITouch = touches.first!
+        let positionInScene = touch.location(in: self)
+        let touchedNode = self.atPoint(positionInScene)
+        
+        if let name = touchedNode.name {
+            print (name)
+            if name == "scout" {
+                print("User is holding finger over scout")
+            }
+        } else {
+            print("User is holding finger over anything else")
             updateScoutRotation(tPoint: positionInScene)
         }
     }
@@ -50,10 +64,6 @@ class GameScene: SKScene {
         let phi = acos(vector_dot(a: a, b: t) / (vector_norm(a: a) * vector_norm(a: t)))
         
         scout.zRotation = t.y > 0 ? phi : -phi;
-        
-        print(a)
-        print(t)
-        print((t.y > 0 ? phi : -phi) * 180 / .pi)
     }
     
     func initBackground() {
