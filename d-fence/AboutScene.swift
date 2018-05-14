4f4649512b9fa5f40ecdf4495184e0275f8f1e0a
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-05-14 09:07:14 +0200

    Beautify code in about scene

--- d-fence/AboutScene.swift
+++ d-fence/AboutScene.swift
@@ -13,70 +13,14 @@ class AboutScene: SKScene {
         backgroundColor = SKColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 1.0)
         
         // Generate labels for every author and the app name
-        
-        let title = SKOutlinedLabelNode(fontNamed: "8-Bit-Madness", fontSize: size.height / 5);
-        title.borderColor = UIColor.black
-        title.borderWidth = title.fontSize / 4.5
-        title.outlinedText = NSLocalizedString("ABOUT", comment: "")
-        title.name = "title"
-        title.fontColor = UIColor.white
-        title.zPosition = 150
-        title.position = CGPoint(x: size.width / 2, y: size.height * 0.8)
-        
-        let back = SKOutlinedLabelNode(fontNamed: "8-Bit-Madness", fontSize: size.height / 10);
-        back.borderColor = UIColor.black
-        back.borderWidth = back.fontSize / 4.5
-        back.outlinedText = NSLocalizedString("BACK", comment: "")
-        back.name = "back"
-        back.fontColor = UIColor.white
-        back.zPosition = 150
-        back.position = CGPoint(x: back.frame.size.width / 2 * 1.2, y: back.frame.size.height / 2)
-        
-        let gameInfo = SKOutlinedLabelNode(fontNamed: "8-Bit-Madness", fontSize: size.height / 10);
-        gameInfo.borderColor = UIColor.black
-        gameInfo.borderWidth = gameInfo.fontSize / 4.5
-        gameInfo.outlinedText = "d-fence © 2018"
-        gameInfo.name = "gameInfo"
-        gameInfo.fontColor = UIColor.white
-        gameInfo.zPosition = 150
-        gameInfo.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        
-        let authorInfo1 = SKOutlinedLabelNode(fontNamed: "8-Bit-Madness", fontSize: size.height / 10);
-        authorInfo1.borderColor = UIColor.black
-        authorInfo1.borderWidth = authorInfo1.fontSize / 4.5
-        authorInfo1.outlinedText = "Jan-Robin Aumann"
-        authorInfo1.name = "authorInfo"
-        authorInfo1.fontColor = UIColor.white
-        authorInfo1.zPosition = 150
-        authorInfo1.position = CGPoint(x: size.width / 2, y: size.height / 3)
-        
-        let authorInfo2 = SKOutlinedLabelNode(fontNamed: "8-Bit-Madness", fontSize: size.height / 10);
-        authorInfo2.borderColor = UIColor.black
-        authorInfo2.borderWidth = authorInfo2.fontSize / 4.5
-        authorInfo2.outlinedText = "Oliver Kardos"
-        authorInfo2.name = "authorInfo"
-        authorInfo2.fontColor = UIColor.white
-        authorInfo2.zPosition = 150
-        authorInfo2.position = CGPoint(x: size.width / 2, y: size.height / 3 - authorInfo2.frame.height * 1.5)
-        
-        let authorInfo3 = SKOutlinedLabelNode(fontNamed: "8-Bit-Madness", fontSize: size.height / 10);
-        authorInfo3.borderColor = UIColor.black
-        authorInfo3.borderWidth = authorInfo3.fontSize / 4.5
-        authorInfo3.outlinedText = "Hendrik Ulbrich"
-        authorInfo3.name = "authorInfo"
-        authorInfo3.fontColor = UIColor.white
-        authorInfo3.zPosition = 150
-        authorInfo3.position = CGPoint(x: size.width / 2, y: size.height / 3 - authorInfo2.frame.height * 3)
-        
-        let authorInfo4 = SKOutlinedLabelNode(fontNamed: "8-Bit-Madness", fontSize: size.height / 17);
-        authorInfo4.borderColor = UIColor.black
-        authorInfo4.borderWidth = authorInfo4.fontSize / 4.5
-        authorInfo4.outlinedText = "Music by Malte Bartels"
-        authorInfo4.name = "authorInfo"
-        authorInfo4.fontColor = UIColor.white
-        authorInfo4.zPosition = 150
-        authorInfo4.position = CGPoint(x: size.width / 2, y: size.height / 3 - authorInfo2.frame.height * 4.5)
-        
+        let title = generateMenuItem(localizedText: "ABOUT", name: "title", fontSize: size.height / 5, vpos: 0.85, hpos: 0.5)
+        let back = generateMenuItem(localizedText: "BACK", name: "back", fontSize: size.height / 10, vpos: 0.05, hpos: 0.1)
+        let gameInfo = generateMenuItem(localizedText: "GAME_CREDIT", name: "gameInfo", fontSize: size.height / 10, vpos: 0.6, hpos: 0.5)
+        let authorInfo1 = generateMenuItem(localizedText: "AUTHOR1_CREDIT", name: "authorInfo", fontSize:size.height / 10, vpos: 0.45, hpos: 0.5);
+        let authorInfo2 = generateMenuItem(localizedText: "AUTHOR2_CREDIT", name: "authorInfo", fontSize:size.height / 10, vpos: 0.35, hpos: 0.5);
+        let authorInfo3 = generateMenuItem(localizedText: "AUTHOR3_CREDIT", name: "authorInfo", fontSize:size.height / 10, vpos: 0.25, hpos: 0.5);
+        let authorInfo4 = generateMenuItem(localizedText: "MUSIC_CREDIT", name: "authorInfo", fontSize:size.height / 15, vpos: 0.15, hpos: 0.5);
+
         addChild(authorInfo1)
         addChild(authorInfo2)
         addChild(authorInfo3)
@@ -98,4 +42,19 @@ class AboutScene: SKScene {
             }
         }
     }
+    
+    // Generic function for rendering about labels
+    func generateMenuItem(localizedText: String, name: String, fontSize: CGFloat, vpos: CGFloat, hpos: CGFloat) -> SKLabelNode {
+        let label = SKOutlinedLabelNode(fontNamed: "8-Bit-Madness", fontSize: fontSize);
+        label.borderColor = UIColor.black
+        label.borderWidth = label.fontSize / 4.5
+        label.outlinedText = NSLocalizedString(localizedText, comment: "")
+        label.name = name
+        label.fontColor = UIColor.white
+        label.zPosition = 150
+        label.position = CGPoint(x: size.width * hpos, y: (size.height - label.frame.height / 2) * vpos)
+        
+        return label
+    }
+    
 }
